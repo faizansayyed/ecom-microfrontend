0afb3094e19cc8162924783b928562727693a27c
faizansayyed <faizansayyed36@gmail.com>
2021-10-27 11:58:53 +0530

    singleton loading

--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -1,22 +1,26 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   devServer: {
     port: 8082,
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'cart',
-      filename: 'remoteEntry.js',
+      name: "cart",
+      filename: "remoteEntry.js",
       exposes: {
-        './CartShow': './src/index',
+        "./CartShow": "./src/index",
       },
-      shared:["faker"]
+      shared: {
+        faker: {
+          singleton: true,
+        },
+      },
     }),
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: "./public/index.html",
     }),
   ],
 };
--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,22 +1,26 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   devServer: {
     port: 8081,
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'products',
-      filename: 'remoteEntry.js',
+      name: "products",
+      filename: "remoteEntry.js",
       exposes: {
-        './ProductsIndex': './src/index',
+        "./ProductsIndex": "./src/index",
       },
-      shared:["faker"]
+      shared: {
+        faker: {
+          singleton: true,
+        },
+      },
     }),
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: "./public/index.html",
     }),
   ],
 };
